e6bc77eda8c7957680d847fefb3f68eabade1c6a
appel <appel.david@live.com>
2017-02-07 15:56:51 -0700

    updated service spec

--- app/specs/issues.service.spec.js
+++ app/specs/issues.service.spec.js
@@ -1,18 +1,9 @@
 "use strict";
+const issues_service_1 = require("../issues.service");
 describe("Issue Service: ", () => {
-    let issueService;
-    /*beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [IssuesService]
-      });
-  
-      issueService = TestBed.createComponent(IssuesService).componentInstance;
-    })
-  
-    describe("GetUrl" , () => {
-  
-      it("Should return last page +1", () => { expect(issueService.getUrl(2)).toContain("page=3")})
-  
-    })*/
+    let issueService = new issues_service_1.IssuesService(null);
+    describe("GetUrl", () => {
+        it("Should return last page +1", () => { expect(issueService.getUrl(2)).toContain("page=3"); });
+    });
 });
 //# sourceMappingURL=issues.service.spec.js.map
--- app/specs/issues.service.spec.ts
+++ app/specs/issues.service.spec.ts
@@ -4,22 +4,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import {IssuesService} from "../issues.service";
+import {Http} from "@angular/http";
 
 describe("Issue Service: ", () => {
 
-  let issueService : IssuesService;
-
-  /*beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [IssuesService]
-    });
-
-    issueService = TestBed.createComponent(IssuesService).componentInstance;
-  })
+  let issueService : IssuesService = new IssuesService(null);
 
   describe("GetUrl" , () => {
-
     it("Should return last page +1", () => { expect(issueService.getUrl(2)).toContain("page=3")})
-
-  })*/
+  })
 })
